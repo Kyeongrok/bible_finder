aaa7a61fce9e4055c9b34caa4a7d7c6c6fa77a5e
mattheu <mattheu@plutusds.com>
2019-04-15 11:42:53 +0900

    add auto kill script

--- deploy_flask.sh
+++ deploy_flask.sh
@@ -1,3 +1,17 @@
+
+BEFORE_PID=$(cat sh.pid)
+
+echo "before PID: $BEFORE_PID"
+
+kill -9 $BEFORE_PID
+
+
 git pull
 nohup python setup.py runserver --host=0.0.0.0 &
+
+echo $! > sh.pid
+
+NEW_PID=$(cat sh.pid)
+echo "NEW PID: $NEW_PID"
+
 ps -ef | grep runserver
