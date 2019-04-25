77010ff01cea095a3f62eed48d5ded4b654d300c
kyeongrok <oceanfog1@gmail.com>
2019-04-25 23:00:32 +0900

    mod bug

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from flask_cors import CORS
 
 import libs.bibleFinder as bf
 from libs.htmlMaker import makeTable
+from libs.htmlMaker import makeTr
 
 # Default JSON encoder는 set를 JSON으로 변환할 수 없다.
 # 그러프로 커스텀 엔코더를 작성해서 set을 list로 변환하여
@@ -43,7 +44,7 @@ def create_app(test_config=None):
         # result = bf.findBetween("창",1, 2, 5)
         # return result[0]['text']
         result = bf.findByIndex(index)
-        return makeHtmlMessage(result[0])
+        return makeTr(result[0])
 
 
     @app.route("/find-between", methods=["GET"])
